cd386c914db95d49b84724ba5854da1ec6c4f2f7
barnosch <31805456+barnosch@users.noreply.github.com>
2025-05-20 12:48:15 +0000

    Update MMM-Lyrion-Cover.js

--- MMM-Lyrion-Cover.js
+++ MMM-Lyrion-Cover.js
@@ -4,67 +4,79 @@ Module.register("MMM-Lyrion-Cover", {
         lmsServer: "http://192.168.1.100:9000",
     },
 
-    start: function() {
+    start: function () {
         this.players = [];
         this.getPlayers();
         setInterval(() => {
             this.getPlayers();
         }, this.config.updateInterval);
     },
-    
-    getTranslations: function () {
-    return {
-      en: "translations/en.json",
-      de: "translations/de.json",
-      };
-    },
 
-    getPlayers: function() {
+    getPlayers: function () {
         this.sendSocketNotification("GET_PLAYERS_AND_TRACKS", this.config.lmsServer);
     },
 
-    socketNotificationReceived: function(notification, payload) {
+    socketNotificationReceived: function (notification, payload) {
         if (notification === "PLAYERS_TRACKS_RESULT") {
             this.players = payload;
             this.updateDom();
         }
     },
 
-    getStyles: function() {
-        return ["MMM-Lyrion.css", "font-awesome.css"];
+    getStyles: function () {
+        return ["MMM-Lyrion-Cover.css", "font-awesome.css"];
     },
 
-    getDom: function() {
-    var wrapper = document.createElement("div");
-    wrapper.className = "Lyrion";
+    getDom: function () {
+        var wrapper = document.createElement("div");
+        wrapper.className = "Lyrion";
 
-    // Nur Player anzeigen, die aktuell etwas abspielen
-    var playingPlayers = this.players.filter(player => player.isPlaying && player.track);
+        var playingPlayers = this.players.filter(player => player.isPlaying && player.track);
 
-    if (playingPlayers.length === 0) {
-        //wrapper.innerHTML = "Kein aktiver Player <i class='fa fa-music'></i>"; //Original
-        wrapper.innerHTML = this.translate("noplayer") + "  <i class='fa fa-music'></i>";
+        if (playingPlayers.length === 0) {
+            wrapper.innerHTML = this.translate("noplayer") + "  <i class='fa fa-music'></i>";
+            return wrapper;
+        }
+
+        playingPlayers.forEach(player => {
+            var playerDiv = document.createElement("div");
+            playerDiv.className = "player-container";
+
+            // Cover links
+            if (player.track && player.track.cover) {
+                var coverImg = document.createElement("img");
+                coverImg.src = player.track.cover;
+                coverImg.alt = "Cover";
+                coverImg.className = "cover-image";
+                coverImg.onerror = function () {
+                    this.style.display = "none";
+                };
+                playerDiv.appendChild(coverImg);
+            }
+
+            // Playerinfo rechts neben Cover
+            var infoDiv = document.createElement("div");
+            infoDiv.className = "player-info";
+
+            var header = document.createElement("div");
+            header.className = "player-header";
+            header.innerHTML = `<b>${player.name}</b>  <i class="fa fa-play"></i>`;
+
+            var trackInfo = document.createElement("div");
+            trackInfo.className = "player-track-info";
+            if (player.track) {
+                trackInfo.innerHTML = `${player.track.artist} – ${player.track.title}`;
+            } else {
+                trackInfo.innerHTML = "";
+            }
+
+            infoDiv.appendChild(header);
+            infoDiv.appendChild(trackInfo);
+
+            playerDiv.appendChild(infoDiv);
+            wrapper.appendChild(playerDiv);
+        });
+
         return wrapper;
-    }
-
-    playingPlayers.forEach(player => {
-        var playerDiv = document.createElement("div");
-        playerDiv.className = "player-container";
-
-        var header = document.createElement("div");
-        header.className = "player-header";
-        header.innerHTML = `<b>${player.name}</b>  <i class="fa fa-play"></i>`;
-        playerDiv.appendChild(header);
-
-        var trackInfo = document.createElement("div");
-        trackInfo.className = "player-track-info";
-        trackInfo.innerHTML = `${player.track.artist} – ${player.track.title}`;
-
-        playerDiv.appendChild(trackInfo);
-        wrapper.appendChild(playerDiv);
-    });
-
-    return wrapper;
-
     }
 });
