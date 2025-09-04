4b49e2d179e56d84286e5125d3a4a20b5a96e789
BicycleRepair-Man <79174016+BicycleRepair-Man@users.noreply.github.com>
2025-09-05 00:50:09 +0200

    Update node_helper.js
    added split selection for artwork url depending on media source

--- node_helper.js
+++ node_helper.js
@@ -8,7 +8,7 @@ module.exports = NodeHelper.create({
 
     socketNotificationReceived: function (notification, payload) {
         if (notification === "GET_PLAYERS_AND_TRACKS") {
-            const lmsServer = payload; // payload ist die Server-URL
+            const lmsServer = payload; // payload is the Server-URL
             this.getPlayersAndTracks(lmsServer);
         }
     },
@@ -42,11 +42,29 @@ module.exports = NodeHelper.create({
                         const track = playlist[0];
                         const trackId = track.id || null;
 
-                        trackInfo = {
-                            title: track.title || "",
-                            artist: track.artist || "",
-                            cover: trackId ? `${lmsServer}/music/${trackId}/cover.jpg` : null
-                        };
+                     let coverUrl = null;
+
+						if (track.artwork_url) {
+                          //remove encoded parts of the URL to be used in next line
+                          let newUrl = track.artwork_url.substring(12, track.artwork_url.length - 10) 
+						  coverUrl = `${lmsServer}/imageproxy/${newUrl}/image.jpg`;
+						} else if (track.coverid) {
+						  // Local file cover
+						  coverUrl = `${lmsServer}/music/${track.coverid}/cover.jpg`;
+						} else if (track.id) {
+						  // Another fallback
+  						  coverUrl = `${lmsServer}/music/${track.id}/cover.jpg`;
+						} else if (player.playerid) {
+ 						  // Last fallback
+ 						  coverUrl = `${lmsServer}/music/current/cover.jpg?player=${encodeURIComponent(player.playerid)}`;
+						}
+
+						trackInfo = {
+  							title: track.title || "",
+  							artist: track.artist || "",
+  							cover: coverUrl,
+							isPlaying: player.isplaying === 1
+						};
                     }
 
                     return {
@@ -69,7 +87,7 @@ module.exports = NodeHelper.create({
             const playersDetails = await Promise.all(playerDetailsPromises);
             this.sendSocketNotification("PLAYERS_TRACKS_RESULT", playersDetails);
         } catch (error) {
-            console.error("Fehler beim Abrufen der Player und Tracks:", error);
+            console.error("Error retrieving status for player:", error);
             this.sendSocketNotification("PLAYERS_TRACKS_RESULT", []);
         }
     }
